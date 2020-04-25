ae2f93718cfa06a5561e4c4a26c56d1c420d9557
Daksh <daksh.sagar@yahoo.com>
2020-04-25 21:16:11 +0530

    Add templates

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,35 +1,69 @@
 package main
+
 import (
+	"database/sql"
 	"flag"
+	"github.com/daksh-sagar/snippetbox/pkg/models/mysql"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
 	errorLog *log.Logger
-	infoLog *log.Logger
+	infoLog  *log.Logger
+	snippets *mysql.SnippetModel
+	templateCache map[string]*template.Template
 }
 
 func main() {
 	addr := flag.String("addr", ":4000", "Http address")
+	dsn := flag.String("dsn", "web:snippetbox@/snippetbox?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime)
 
+	db, err := openDB(*dsn)
+	if err != nil {
+		errLog.Fatal(err)
+	}
+
+	defer db.Close()
+
+	templateCache, err := newTemplateCache("./ui/html")
+	if err != nil {
+		errLog.Fatal(err)
+	}
+
 	app := &application{
 		errorLog: errLog,
 		infoLog:  infoLog,
+		snippets: &mysql.SnippetModel{DB: db},
+		templateCache: templateCache,
 	}
 
 	server := &http.Server{
-		Addr: *addr,
+		Addr:     *addr,
 		ErrorLog: errLog,
-		Handler: app.routes(),
+		Handler:  app.routes(),
 	}
 
 	infoLog.Printf("Starting server on %s\n", *addr)
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 	errLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
