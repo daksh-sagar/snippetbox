d5e5e8b7b0a9949df42efe4bf321946efab04669
Daksh <daksh.sagar@yahoo.com>
2020-04-27 19:22:45 +0530

    Add create snippet form and form validation

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -6,6 +6,8 @@ import (
 	"github.com/daksh-sagar/snippetbox/pkg/models"
 	"net/http"
 	"strconv"
+	"strings"
+	"unicode/utf8"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
@@ -38,14 +40,44 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func (app *application) createSnippetForm(w http.ResponseWriter, _ *http.Request) {
-	_, _ = w.Write([]byte("Create a new Snippet..."))
+func (app *application) createSnippetForm(w http.ResponseWriter, r *http.Request) {
+	app.render(w, r, "create.page.tmpl", nil)
 }
 
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
-	title := "O snail"
-	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
-	expires := "7"
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	title := r.PostForm.Get("title")
+	content := r.PostForm.Get("content")
+	expires := r.PostForm.Get("expires")
+
+	formErrors := make(map[string]string)
+
+	if strings.TrimSpace(title) == "" {
+		formErrors["title"] = "This field cannot be blank"
+	} else if utf8.RuneCountInString(title) > 100 {
+		formErrors["title"] = "The max length for this field is 100 characters"
+	}
+	if strings.TrimSpace(content) == "" {
+		formErrors["content"] = "This field cannot be blank"
+	}
+	if strings.TrimSpace(expires) == "" {
+		formErrors["expires"] = "This field cannot be blank"
+	} else if expires != "365" && expires != "7" && expires != "1" {
+		formErrors["expires"] = "This field is invalid"
+	}
+
+	if len(formErrors) > 0 {
+		app.render(w, r, "create.page.tmpl", &templateData{
+			FormErrors: formErrors,
+			FormData:   r.PostForm,
+		})
+		return
+	}
 
 	id, err := app.snippets.Insert(title, content, expires)
 	if err != nil {
@@ -53,5 +85,5 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
+	http.Redirect(w, r, fmt.Sprintf("/snippet/%d", id), http.StatusSeeOther)
 }
