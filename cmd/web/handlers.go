157d47fdc67449c09d59dc7335f4e2725e295830
Daksh <daksh.sagar@yahoo.com>
2020-04-29 03:06:48 +0530

    Encapsulate form validation functionality to a separate forms package

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,11 +3,10 @@ package main
 import (
 	"errors"
 	"fmt"
+	"github.com/daksh-sagar/snippetbox/pkg/forms"
 	"github.com/daksh-sagar/snippetbox/pkg/models"
 	"net/http"
 	"strconv"
-	"strings"
-	"unicode/utf8"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
@@ -41,7 +40,7 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) createSnippetForm(w http.ResponseWriter, r *http.Request) {
-	app.render(w, r, "create.page.tmpl", nil)
+	app.render(w, r, "create.page.tmpl", &templateData{Form: forms.New(nil)})
 }
 
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
@@ -51,35 +50,17 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	title := r.PostForm.Get("title")
-	content := r.PostForm.Get("content")
-	expires := r.PostForm.Get("expires")
+	form := forms.New(r.PostForm)
+	form.Required("title", "content", "expires")
+	form.MaxLength("title", 100)
+	form.PermittedValues("expires", "365", "7", "1")
 
-	formErrors := make(map[string]string)
-
-	if strings.TrimSpace(title) == "" {
-		formErrors["title"] = "This field cannot be blank"
-	} else if utf8.RuneCountInString(title) > 100 {
-		formErrors["title"] = "The max length for this field is 100 characters"
-	}
-	if strings.TrimSpace(content) == "" {
-		formErrors["content"] = "This field cannot be blank"
-	}
-	if strings.TrimSpace(expires) == "" {
-		formErrors["expires"] = "This field cannot be blank"
-	} else if expires != "365" && expires != "7" && expires != "1" {
-		formErrors["expires"] = "This field is invalid"
-	}
-
-	if len(formErrors) > 0 {
-		app.render(w, r, "create.page.tmpl", &templateData{
-			FormErrors: formErrors,
-			FormData:   r.PostForm,
-		})
+	if !form.Valid() {
+		app.render(w, r, "create.page.tmpl", &templateData{Form: form})
 		return
 	}
 
-	id, err := app.snippets.Insert(title, content, expires)
+	id, err := app.snippets.Insert(form.Get("title"), form.Get("content"), form.Get("expires"))
 	if err != nil {
 		app.serverError(w, err)
 		return
